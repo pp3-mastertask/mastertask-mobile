928276b99f2ec6e762b9220a3210674e666b9ae0
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2023-09-02 22:15:12 -0300

    build: successfully connected to database
    - showed results in textview

--- mastertask/app/src/main/java/com/example/mastertask/MainActivity.kt
+++ mastertask/app/src/main/java/com/example/mastertask/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mastertask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var lbTeste : TextView
+
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lbTeste = findViewById(R.id.lbTeste)
+
+        db.collection("usuarios").get().addOnSuccessListener {
+            result ->
+            for (document in result) {
+                lbTeste.text = lbTeste.text.toString().plus("${document.data}")
+            }
+        }
     }
 }
