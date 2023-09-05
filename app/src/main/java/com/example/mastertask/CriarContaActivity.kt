d3bd6afe73dcc592a7fee592b03bbc10e6a6aec9
eric-figueira <eric.figueira.07@gmail.com>
2023-09-05 11:23:47 -0300

    feat: add registration data page
    Fixed main page button

---
+++ app/src/main/java/com/example/mastertask/CriarContaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mastertask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriarContaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_conta)
+    }
+}
