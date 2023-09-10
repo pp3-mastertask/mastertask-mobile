9041e5e5883c1d8bf4c888d4f2a12a9ffb1015e9
Marcos-Godinho-Filho <mcvsk.filho@gmail.com>
2023-09-10 12:43:57 -0300

    build: menu and empty pages
    - created those implementations initially in branch home, but as we're considering each branch as a screen/feature, I've decided to bring that to main

---
+++ app/src/main/java/com/example/mastertask/Application.kt
@@ -0,0 +1,41 @@
+package com.example.mastertask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Application : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.navigation_view)
+        bottomNav.setOnItemSelectedListener(navListener)
+
+        // as soon as the application opens the first fragment should
+        // be shown to the user in this case it is home fragment
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+    }
+
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        // By using switch we can easily get the
+        // selected fragment by using there id
+        lateinit var selectedFragment: Fragment
+        when (it.itemId) {
+            R.id.home -> {
+                selectedFragment = HomeFragment()
+            }
+            R.id.servicos -> {
+                selectedFragment = ServicesFragment()
+            }
+            R.id.user -> {
+                selectedFragment = UserFragment()
+            }
+        }
+        // It will help to replace the
+        // one fragment to other.
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+        true
+    }
+}
--- app/src/main/java/com/example/mastertask/MainActivity.kt
+++ app/src/main/java/com/example/mastertask/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mastertask
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var lbTeste : TextView
@@ -23,5 +25,8 @@ class MainActivity : AppCompatActivity() {
                 lbTeste.text = lbTeste.text.toString().plus("${document.data}")
             }
         }
+
+        val intent = Intent(this, Application::class.java)
+        startActivity(intent)
     }
 }
