91ef352981414ab4b0fe74f1bf69e9b2f1cf2cb5
eric-figueira <eric.figueira.07@gmail.com>
2023-09-13 10:10:45 -0300

    chore: improve methods structure

--- app/src/main/java/com/example/mastertask/EditarContaActivity.kt
+++ app/src/main/java/com/example/mastertask/EditarContaActivity.kt
@@ -11,34 +11,71 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mastertask.Adapters.EditAccountSkillsAdapter
 import com.example.mastertask.Models.SkillModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
 
 class EditarContaActivity : AppCompatActivity() {
 
-    private lateinit var btnAddSkill: Button
+    private lateinit var btnAddSkill          : Button
+    private lateinit var btnConfirmEditAccount: Button
+    private lateinit var btnCancelEditAccount : Button
+
+    private lateinit var txtCpf       : EditText
+    private lateinit var txtNascimento: EditText
+    private lateinit var txtContato   : EditText
+    private lateinit var txtLocalidade: EditText
+    private lateinit var dtDisponibilidade: EditText
 
     private lateinit var lbAuthTest: TextView
+
+    private var currentUser = FirebaseAuth.getInstance().currentUser
 
     private var list_skills = ArrayList<SkillModel>()
     private lateinit var recyclerAdapter:EditAccountSkillsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_criar_conta)
+        setContentView(R.layout.activity_editar_conta)
 
         val email = intent.getStringExtra("email")
 
         lbAuthTest = findViewById(R.id.txtTesteAuth)
         lbAuthTest.text = email
 
-        this.btnAddSkill = this.findViewById(R.id.criarConta_addSkill)
-        btnAddSkill.setOnClickListener({ createAddSkillDialog() })
+        this.configureElements()
+        this.configureRecyclerView()
 
+        this.fillInputs()
+    }
+
+    private fun configureElements() {
+        this.btnAddSkill           = this.findViewById(R.id.editarConta_btnAddSkill)
+        this.btnConfirmEditAccount = this.findViewById(R.id.editarConta_btnConfirmar)
+        this.btnCancelEditAccount  = this.findViewById(R.id.editarConta_btnCancelar)
+
+        this.txtCpf        = findViewById(R.id.editarConta_txtCpf)
+        this.txtNascimento = findViewById(R.id.editarConta_txtDataNascimento)
+        this.txtContato    = findViewById(R.id.editarConta_txtTelefone)
+        this.txtLocalidade = findViewById(R.id.editarConta_txtLocalidade)
+        this.dtDisponibilidade = findViewById(R.id.editarConta_calendarioDisponibilidade)
+
+        this.setEventListeners()
+    }
+
+    private fun configureRecyclerView() {
         this.recyclerAdapter = EditAccountSkillsAdapter(this.list_skills)
 
-        val recyclerView: RecyclerView = findViewById(R.id.criarConta_recyclerView)
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = recyclerAdapter
+        val recyclerView: RecyclerView = findViewById(R.id.editarConta_recyclerView)
+        val layoutManager              = LinearLayoutManager(this)
+        recyclerView.layoutManager     = layoutManager
+        recyclerView.adapter           = this.recyclerAdapter
+    }
+
+    private fun setEventListeners() {
+        this.btnAddSkill          .setOnClickListener({ createAddSkillDialog() })
+        this.btnCancelEditAccount .setOnClickListener({ this.handleCancelEditAccount() })
+        this.btnConfirmEditAccount.setOnClickListener({ this.handleConfirmEditAccount() })
     }
 
     private fun createAddSkillDialog() {
@@ -50,8 +87,8 @@ class EditarContaActivity : AppCompatActivity() {
 
         val txtNomeSkill: EditText = dg.findViewById(R.id.add_skill_name)
         val txtPrecoSkill: EditText = dg.findViewById(R.id.add_skill_price)
-        val btnCancelar: Button = dg.findViewById(R.id.add_service_cancel)
-        val btnAdicionarSkill: Button = dg.findViewById(R.id.add_service_confirm)
+        val btnCancelar: Button = dg.findViewById(R.id.add_skill_cancel)
+        val btnAdicionarSkill: Button = dg.findViewById(R.id.add_skill_confirm)
 
         btnCancelar.setOnClickListener({ dg.cancel() })
 
@@ -68,5 +105,27 @@ class EditarContaActivity : AppCompatActivity() {
         }
 
         dg.show()
+    }
+
+    private fun handleCancelEditAccount() {
+        // redirecionar para a primeira pagina de edicao de usuario
+    }
+
+    private fun handleConfirmEditAccount() {
+        val userUpdatedData = hashMapOf(
+            "contato" to txtContato.text.toString(),
+            "cpf" to txtCpf.text.toString(),
+            "dataNascimento" to null,
+            "disponibilidade" to "",
+            "endereco" to "",
+            "habilidades" to null,
+            "mediaAtual" to 0.0,
+            "nome" to "",
+            "numServicosFeitos" to 0
+        )
+    }
+
+    private fun fillInputs() {
+        // testar se o usuario tem dados, se tiver, preencher os inputs
     }
 }
