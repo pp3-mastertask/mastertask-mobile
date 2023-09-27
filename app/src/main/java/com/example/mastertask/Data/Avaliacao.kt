dbb9fb82394afe3d660714ef915fd8bb58d0e2b1
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2023-09-27 09:56:15 -0300

    feat; data and model classes

---
+++ app/src/main/java/com/example/mastertask/Data/Avaliacao.kt
@@ -0,0 +1,22 @@
+package com.example.mastertask.Data
+
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Avaliacao(
+    var id: String? = null,
+    var comentario: String? = null,
+    var estrelas: Double? = null,
+    var servico: String? = null
+) {
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "id" to id,
+            "comentario" to comentario,
+            "estrelas" to estrelas,
+            "servico" to servico
+        )
+    }
+}
