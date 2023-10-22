d64459fc245dc1d6c90cc1068a2ab882cd39da02
URICH <107805174+BennethUrich@users.noreply.github.com>
2023-10-22 03:11:20 -0300

    update service confirm client :D
    socorro esta tudo errado akqkkkkkk

--- app/src/main/java/com/example/mastertask/Fragments/ServiceConfirmClient.kt
+++ app/src/main/java/com/example/mastertask/Fragments/ServiceConfirmClient.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.example.mastertask.Data.Service
+import com.example.mastertask.Data.User
 import com.example.mastertask.Models.HabilidadeViewModel
+import com.example.mastertask.Models.ServiceViewModel
 import com.example.mastertask.Models.UserViewModel
 import com.example.mastertask.R
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,13 +33,25 @@ class ServiceConfirmClient : Fragment() {
     private lateinit var userViewModel: UserViewModel
     private lateinit var serviceViewModel: HabilidadeViewModel
 
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var serviceViewModel: ServiceViewModel
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var selectedServices: List<Service>
+    private lateinit var currentUser: User
+
+    private lateinit var textViewCliente: eu n sei como pega essas ifos aqkkkkkk
+    private lateinit var textViewTotalPrice:eu n sei como pega essas ifos aqkkkkkk
+    private lateinit var buttonSolicitar:eu n sei como pega essas ifos aqkkkkkk
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        //super.onCreate(savedInstanceState)
+        //arguments?.let {
+        //    param1 = it.getString(ARG_PARAM1)
+        //    param2 = it.getString(ARG_PARAM2)
+
+
     }
 
     override fun onCreateView(
@@ -41,8 +59,51 @@ class ServiceConfirmClient : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_service_confirm_client, container, false)
+
+
+        userViewModel = UserViewModel()
+        serviceViewModel = ServiceViewModel()
+        auth = FirebaseAuth.getInstance()
+
+        val currentUserEmail = auth.currentUser?.email
+        currentUser = userViewModel.getUserByEmail(currentUserEmail) ?: User()
+
+
+        selectedServices = // eu realmente n achei nada sobre como fazkkkk vou procurar mais um pouco ainddakkkkkkkk
+
+        //inicializa as views
+        textViewCliente = view.findViewById(R.id.text_cliente) //n lembro como chamakkkkk ja ja eu olhokkk
+        textViewTotalPrice = view.findViewById(R.id.text_total_price)
+        buttonSolicitar = view.findViewById(R.id.button_solicitar)
+
+        setupUI()//inicializa a interface do usuario???
+
+        buttonSolicitar.setOnClickListener {
+            criarServico()
+        }
         return inflater.inflate(R.layout.fragment_service_confirm_client, container, false)
     }
+
+    private fun setupUI() {
+        textViewCliente.text = "Cliente: ${currentUser.nome}"
+
+        val totalPrice = calcularPrecoTotal(selectedServices)
+        textViewTotalPrice.text = "Total a pagar: R$ $totalPrice"
+    }
+
+    private fun calcularPrecoTotal(selectedServices: List<Service>): Double {
+        var totalPrice = 0.0
+        for (service in selectedServices) {
+            totalPrice += service.preco
+        }
+        return totalPrice
+    }
+
+    private fun criarServico() {
+        
+    }
+}
 
     companion object {
         /**
