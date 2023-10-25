ca8bbb22d66cabfd783bf0a21a86fe6163f7dad4
Marcos-Godinho-Filho <mcvsk.filho@gmail.com>
2023-10-24 21:01:25 -0300

    edit account layout and profile pics

--- app/src/main/java/com/example/mastertask/Fragments/UserFragment.kt
+++ app/src/main/java/com/example/mastertask/Fragments/UserFragment.kt
@@ -23,6 +23,7 @@ import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 
 
@@ -88,6 +89,8 @@ class UserFragment : Fragment() {
     fun addValues() {
         val currentUser = this.auth.currentUser!!
         val userEmail = currentUser.email.toString()
+
+        Picasso.get().load(currentUser.photoUrl.toString()).into(imgPhoto)
 
         var currentUserData: Map<String, Any>? = null
 
